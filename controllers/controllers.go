63c7c3f83c52309c0e1e742c01bbdf06b174f524
EuAndersonRoberto <euandersondigital@gmail.com>
2022-12-21 21:54:33 -0300

    modularizing the code

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,6 +2,7 @@ package controllers
 
 import "github.com/gin-gonic/gin"
 
+// Aqui definimos todos as funções que utilizaremos no projeto mas, poderiamos colocar cada função em um file específico dentro da pasta controllers, modularizando ainda mais nosso código. Neste projeto faremos todas as funções dentro deste único file.
 func ExibeTodosAlunos(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"id":   "1",
