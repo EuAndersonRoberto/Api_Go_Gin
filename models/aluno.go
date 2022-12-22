2720ec06c52a43a8b353f564c3c70e3f43f593ab
EuAndersonRoberto <euandersondigital@gmail.com>
2022-12-22 17:25:32 -0300

    Created with DB

--- models/aluno.go
+++ models/aluno.go
@@ -1,9 +1,12 @@
 package models
 
+import "gorm.io/gorm"
+
 type Aluno struct {
-	Nome string `json:"nome"`
-	CPF  string `json:"cpf"`
-	RG   string `json:"rg"`
+	gorm.Model        // Este comando nos trás uma struct do GORM que já nos apresenta: id, create, update e delete.
+	Nome       string `json:"nome"`
+	CPF        string `json:"cpf"`
+	RG         string `json:"rg"`
 }
 
 var Alunos []Aluno //Aqui criamos uma variável Alunos que comtém um slice da struct "Aluno".
