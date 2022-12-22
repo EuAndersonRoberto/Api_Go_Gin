91cd053831785e547b88ad955ab06b091bda64c6
EuAndersonRoberto <euandersondigital@gmail.com>
2022-12-21 21:40:22 -0300

    Endpoint created

--- main.go
+++ main.go
@@ -4,7 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "Anderson Roberto",
+	})
+}
+
 func main() {
 	r := gin.Default()
+	r.GET("/alunos", ExibeTodosAlunos) // Aqui definimos que toda requisão GET feita para o endpoint: /alunos, irá responder com a função "ExibeTodosAlunos", nos devolvendo o código 200 e a mensagem JSON: "id":"1" e "nome":"Anderson Roberto", na URL: http://localhost:8080/alunos
 	r.Run()
 }
